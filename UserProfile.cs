83452c5ff6929b8ba0489f7f52a649d581686eae
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2020-02-08 19:11:00 +0000

    changes to user Profile

--- UserProfile.cs
+++ UserProfile.cs
@@ -1,4 +1,5 @@
 // stores user's profile 
+using System;
 using System.Collections.Generic;
 
 public class UserProfile
@@ -6,5 +7,7 @@ public class UserProfile
     public string Name { get; set; }
   
     // The list of companies the user wants to review.
+
+    public Nullable<int> NumberOfModules {get; set;}
     public List<string> ModulesTaken { get; set; } = new List<string>();
 }
