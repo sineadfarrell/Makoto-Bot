7a424488acd1fadb9ac579800fa6f9909b355b52
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2020-02-27 18:18:49 +0000

    update to Extracurricular dialogs

--- Dialogs/ExtracurricularDialog.cs
+++ Dialogs/ExtracurricularDialog.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
+using System; 
+using Linq;
 using Microsoft.Extensions.Logging;
 using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
 
@@ -11,14 +13,14 @@ namespace Microsoft.BotBuilderSamples.Dialogs
 {
     public class ExtracurricularDialog : ComponentDialog
     {
-         private readonly ConversationRecognizer _luisRecognizer;
+        private readonly ConversationRecognizer _luisRecognizer;
         protected readonly ILogger Logger;
-        
-        
-        public ExtracurricularDialog(ConversationRecognizer luisRecognizer,  ILogger<ExtracurricularDialog> logger, EndConversationDialog endConversationDialog)
+
+
+        public ExtracurricularDialog(ConversationRecognizer luisRecognizer, ILogger<ExtracurricularDialog> logger, EndConversationDialog endConversationDialog)
             : base(nameof(ExtracurricularDialog))
 
-        {   
+        {
             _luisRecognizer = luisRecognizer;
             Logger = logger;
             AddDialog(new TextPrompt(nameof(TextPrompt)));
@@ -28,14 +30,14 @@ namespace Microsoft.BotBuilderSamples.Dialogs
                 IntroStepAsync,
                 GetInfoAsync,
                 MoveConvoAsync,
-                
+
             }));
 
             // The initial child Dialog to run.
             InitialDialogId = nameof(WaterfallDialog);
         }
 
-         private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             if (!_luisRecognizer.IsConfigured)
             {
@@ -45,32 +47,35 @@ namespace Microsoft.BotBuilderSamples.Dialogs
                 return await stepContext.NextAsync(null, cancellationToken);
             }
             var luisResult = await _luisRecognizer.RecognizeAsync<Luis.Conversation>(stepContext.Context, cancellationToken);
-           
-           //if the user has said they do an activity talk about that rather than asking again
-           var userDetails = new UserProfile(){
-               Name = luisResult.Entities.UserName,
-               Activity = luisResult.Entities.Extracurricular, 
-           };
-           var messageText = $"";
-        var elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput)};
-           
-           if(userDetails.Activity.Equals(null)){
-             messageText = $"Great! So what kinda things do you like doing on campus?";
-            elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput)};
-           }
-           else{
-            messageText = $"That's very cool! Are you a part of a team or a club for {userDetails.Activity}?";
-            elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput)};
-           }
-          
-            
+
+            //if the user has said they do an activity talk about that rather than asking again
+            var userDetails = new UserProfile()
+            {
+                Name = luisResult.Entities.UserName,
+                Activity = luisResult.Entities.Extracurricular,
+            };
+            var messageText = $"";
+            var elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput) };
+
+            if (string.IsNullOrEmpty(userDetails.Activity.First()))
+            {
+                messageText = $"Great! So what kinda things do you like doing on campus?";
+                elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput) };
+            }
+            else
+            {
+                messageText = $"That's very cool! Are you a part of a team or a club for {userDetails.Activity}?";
+                elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput) };
+            }
+
+
             return await stepContext.PromptAsync(nameof(TextPrompt), elsePromptMessage, cancellationToken);
         }
 
 
         private async Task<DialogTurnResult> GetInfoAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-             if (!_luisRecognizer.IsConfigured)
+            if (!_luisRecognizer.IsConfigured)
             {
                 await stepContext.Context.SendActivityAsync(
                     MessageFactory.Text("NOTE: LUIS is not configured. To enable all capabilities, add 'LuisAppId', 'LuisAPIKey' and 'LuisAPIHostName' to the web.config file.", inputHint: InputHints.IgnoringInput), cancellationToken);
@@ -78,30 +83,33 @@ namespace Microsoft.BotBuilderSamples.Dialogs
                 return await stepContext.NextAsync(null, cancellationToken);
             }
 
-          var luisResult = await _luisRecognizer.RecognizeAsync<Luis.Conversation>(stepContext.Context, cancellationToken);
-           
-            var moduleDetails = new ModuleDetails(){
+            var luisResult = await _luisRecognizer.RecognizeAsync<Luis.Conversation>(stepContext.Context, cancellationToken);
+
+            var moduleDetails = new ModuleDetails()
+            {
                 Lecturer = luisResult.Entities.Lecturer,
                 Opinion = luisResult.Entities.Opinion,
             };
-            
-             if(luisResult.TopIntent().Equals(Luis.Conversation.Intent.endConversation)){
-                return await stepContext.BeginDialogAsync(nameof(EndConversationDialog), moduleDetails, cancellationToken);;    
-           }
-           if(luisResult.TopIntent().Equals(Luis.Conversation.Intent.discussExtracurricular)){
-            var messageText = $"Wow that's great!";
-            var elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput)};
-            await stepContext.PromptAsync(nameof(TextPrompt), elsePromptMessage, cancellationToken);
-        }
-           
+
+            if (luisResult.TopIntent().Equals(Luis.Conversation.Intent.endConversation))
+            {
+                return await stepContext.BeginDialogAsync(nameof(EndConversationDialog), moduleDetails, cancellationToken); ;
+            }
+            if (luisResult.TopIntent().Equals(Luis.Conversation.Intent.discussExtracurricular))
+            {
+                var messageText = $"Wow that's great!";
+                var elsePromptMessage = new PromptOptions { Prompt = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput) };
+                await stepContext.PromptAsync(nameof(TextPrompt), elsePromptMessage, cancellationToken);
+            }
+
             var messageText2 = $"Is there anything else you'd like to talk about?";
-            var elsePromptMessage2 = new PromptOptions { Prompt = MessageFactory.Text(messageText2, messageText2, InputHints.ExpectingInput)};
+            var elsePromptMessage2 = new PromptOptions { Prompt = MessageFactory.Text(messageText2, messageText2, InputHints.ExpectingInput) };
             return await stepContext.PromptAsync(nameof(TextPrompt), elsePromptMessage2, cancellationToken);
         }
-    
-    private async Task<DialogTurnResult> MoveConvoAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+
+        private async Task<DialogTurnResult> MoveConvoAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-             if (!_luisRecognizer.IsConfigured)
+            if (!_luisRecognizer.IsConfigured)
             {
                 await stepContext.Context.SendActivityAsync(
                     MessageFactory.Text("NOTE: LUIS is not configured. To enable all capabilities, add 'LuisAppId', 'LuisAPIKey' and 'LuisAPIHostName' to the web.config file.", inputHint: InputHints.IgnoringInput), cancellationToken);
@@ -109,12 +117,13 @@ namespace Microsoft.BotBuilderSamples.Dialogs
                 return await stepContext.NextAsync(null, cancellationToken);
             }
             var luisResult = await _luisRecognizer.RecognizeAsync<Luis.Conversation>(stepContext.Context, cancellationToken);
-           
-        if(luisResult.Text.Equals("no")){
-            return await stepContext.BeginDialogAsync(nameof(EndOfConversationCodes), cancellationToken);
-        }
-          return await stepContext.EndDialogAsync();
 
-    }
+            if (luisResult.Text.Equals("no"))
+            {
+                return await stepContext.BeginDialogAsync(nameof(EndOfConversationCodes), cancellationToken);
+            }
+            return await stepContext.EndDialogAsync();
+
+        }
     }
 }
