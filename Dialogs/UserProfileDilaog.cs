5bde895de7f3a9056fad8fb5e87cb35b50c36abe
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2020-03-13 17:02:30 +0000

    Update UserProfileDialog

--- Dialogs/UserProfileDilaog.cs
+++ Dialogs/UserProfileDilaog.cs
@@ -73,13 +73,14 @@ namespace Microsoft.BotBuilderSamples.Dialogs
             if(luisResult.TopIntent().Equals(Luis.Conversation.Intent.None)){
                     var didntUnderstandMessageText = $"Sorry, I didn't get that. Please try rephrasing your message(intent was {luisResult.TopIntent().intent})";
                     var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
-                    return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = didntUnderstandMessage }, cancellationToken);
-            }
+                    await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = didntUnderstandMessage }, cancellationToken);
+                    return await stepContext.ReplaceDialogAsync(nameof(MainDialog));            }
 
             if((userInfo.Name.FirstOrDefault()).Equals("")){
                     var didntUnderstandMessageText = $"Sorry, I didn't get that. Please try rephrasing your message(intent was {luisResult.TopIntent().intent})";
                     var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
-                    return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = didntUnderstandMessage }, cancellationToken);
+                     await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = didntUnderstandMessage }, cancellationToken);
+                    return await stepContext.ReplaceDialogAsync(nameof(MainDialog));
             }
                 
                  await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Thanks {userInfo.Name.FirstOrDefault()}, it's great to meet you!"), cancellationToken);
