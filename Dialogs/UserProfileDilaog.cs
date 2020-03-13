31c2fc66f2a456d3a6369fd775acd9f03ae24b98
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2020-03-13 16:13:18 +0000

    fix to UserProfile

--- Dialogs/UserProfileDilaog.cs
+++ Dialogs/UserProfileDilaog.cs
@@ -76,7 +76,7 @@ namespace Microsoft.BotBuilderSamples.Dialogs
                     return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = didntUnderstandMessage }, cancellationToken);
             }
 
-            if(string.IsNullOrWhiteSpace(userInfo.Name.FirstOrDefault())){
+            if((userInfo.Name.FirstOrDefault()).Equals("")){
                     var didntUnderstandMessageText = $"Sorry, I didn't get that. Please try rephrasing your message(intent was {luisResult.TopIntent().intent})";
                     var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
                     return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = didntUnderstandMessage }, cancellationToken);
