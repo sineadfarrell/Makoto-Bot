3c4902e90a049d2cac860e919a615579998532a9
SineadFarrell <sinead.farrell1@ucdconnect.ie>
2020-03-16 19:00:51 +0000

    fix

--- Dialogs/UserProfileDilaog.cs
+++ Dialogs/UserProfileDilaog.cs
@@ -15,7 +15,7 @@ namespace Microsoft.BotBuilderSamples.Dialogs
         private readonly ConversationRecognizer _luisRecognizer;
         protected readonly ILogger Logger;
 
-       public UserProfileDialog(ConversationRecognizer luisRecognizer, UserProfileDialog userProfileDialog, ILogger<UserProfileDialog> logger, EndConversationDialog endConversationDialog, ModuleDialog moduleDialog)
+       public UserProfileDialog(ConversationRecognizer luisRecognizer, ILogger<UserProfileDialog> logger, EndConversationDialog endConversationDialog, ModuleDialog moduleDialog)
             : base(nameof(UserProfileDialog))
         {
             // _userProfileAccessor = userState.CreateProperty<UserProfile>("UserProfile");
@@ -25,7 +25,7 @@ namespace Microsoft.BotBuilderSamples.Dialogs
             AddDialog(new TextPrompt(nameof(TextPrompt)));
             AddDialog(endConversationDialog);
             AddDialog(moduleDialog);
-            AddDialog(userProfileDialog);
+            
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
             {
 
